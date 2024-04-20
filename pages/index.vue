9d047ae39c3b9b783d0e9378125dd01e919c6300
Albertus Marco <99940477+marcoruslie@users.noreply.github.com>
2024-04-20 16:15:55 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,29 @@
 <template>
   <div>
     <!-- <slide-show-design :fileList="fileList" :currentIndex="currentIndex" :leaving="leaving" :currentItem="currentItem" /> -->
-    <two-side-design 
-      :kategori="'lowongan'"
-      :dataList="fileList" 
-      :currentItem="currentLowongan" />
+    <!-- <two-side-design :kategori="'lowongan'" :dataList="fileList" :currentItem="currentLowongan" /> -->
+    <h1>
+      {{ dataFromServer }}
+    </h1>
   </div>
 </template>
 
 <script setup lang="ts">
 import { io } from 'socket.io-client';
+const dataFromServer = ref('')
 const { getLaporanBAK, getLowongan, getPengumuman } = useDataISTTS()
 const { getFileList } = useFileData();
 const fileList = ref(await getFileList())
 const currentIndex = ref(0);
 const leaving = ref(false);
 const currentItem: any = computed(() => fileList.value[currentIndex.value] || {});
-const [pengumumanRes, lowonganRes, laporanBAKRes]:any = await Promise.all([getPengumuman(), getLowongan(), getLaporanBAK()])
+const [pengumumanRes, lowonganRes, laporanBAKRes]: any = await Promise.all([getPengumuman(), getLowongan(), getLaporanBAK()])
 const dataPengumuman = pengumumanRes.response
 const dataLowongan = lowonganRes.response
 const dataLaporanBAK = laporanBAKRes.response
-const currentPengumuman:any = computed(() => dataPengumuman[currentIndex.value] || {});
-const currentLowongan:any = computed(() => dataLowongan[currentIndex.value] || {});
-const currentLaporanBAK:any = computed(() => dataLaporanBAK[currentIndex.value] || {});
+const currentPengumuman: any = computed(() => dataPengumuman[currentIndex.value] || {});
+const currentLowongan: any = computed(() => dataLowongan[currentIndex.value] || {});
+const currentLaporanBAK: any = computed(() => dataLaporanBAK[currentIndex.value] || {});
 
 onMounted(async () => {
 
@@ -38,6 +39,14 @@ onMounted(async () => {
   socket.on('tes', (response: Record<string, string>) => {
     console.log(response);
   })
+  socket.on('tes2', (response: Record<string, string>) => {
+    dataFromServer.value = response.message
+  })
+  socket.on('sendData', (response: Record<string, string>) => {
+    console.log(response);
+    dataFromServer.value = response.message
+  })
+  
   socket.on('refresh', () => {
     useRouter().go(0);
   })
