fb781f9a5b174bc0253327f727bd99a26f65bc33
Albertus Marco <99940477+marcoruslie@users.noreply.github.com>
2024-09-30 11:36:40 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -84,7 +84,7 @@ const currentItem3 = ref("")
 // THREE SIDE DESIGN VARIABLES
 
 const host = "http://192.168.9.203/"
-// const host = "http://10.10.4.210:3000/"
+// const host = "http://192.168.0.190:3000/"
 // const host = "http://localhost:3000/"
 const socket = io(host, {
 	path: "/api/socket.io",
