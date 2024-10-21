51be3c31e7f91489605ad79a0af889e92e1ff84a
Albertus Marco <99940477+marcoruslie@users.noreply.github.com>
2024-10-21 09:17:00 +0700

    Update SlideShowPortrait.vue

--- components/SlideShowPortrait.vue
+++ components/SlideShowPortrait.vue
@@ -2,7 +2,8 @@
     <div
         class="bg-gradient-to-tl from-Primary to-OnPrimaryContainer overflow-hidden h-[97vw] w-[100vh] flex justify-center items-center">
         <img v-if="currentItem1.type === 'image'" :src="'/_nuxt/' + currentItem1.url" class="w-full" />
-        <video v-else-if="currentItem1.type === 'video'" autoplay muted class="h-full">
+        <video v-else-if="currentItem1.type === 'video'" autoplay controls playsinline class="h-full"
+            @canplay="playVideo">
             <source :src="'/_nuxt/' + currentItem1.url" type="video/mp4" />
         </video>
     </div>
@@ -10,4 +11,10 @@
 
 <script setup>
 const { currentItem1 } = defineProps(["currentItem1"])
+const playVideo = (event) => {
+    const video = event.target;
+    video.play().catch((error) => {
+        console.warn("Autoplay blocked:", error);
+    });
+};
 </script>
