3f9ff20f2b30222710e6fbb9fbb80c2d84947024
Albertus Marco <99940477+marcoruslie@users.noreply.github.com>
2024-10-21 09:13:47 +0700

    Update SlideShowPortrait.vue

--- components/SlideShowPortrait.vue
+++ components/SlideShowPortrait.vue
@@ -2,7 +2,7 @@
     <div
         class="bg-gradient-to-tl from-Primary to-OnPrimaryContainer overflow-hidden h-[97vw] w-[100vh] flex justify-center items-center">
         <img v-if="currentItem1.type === 'image'" :src="'/_nuxt/' + currentItem1.url" class="w-full" />
-        <video v-else autoplay class="h-full">
+        <video v-else-if="currentItem1.type === 'video'" autoplay class="h-full">
             <source :src="'/_nuxt/' + currentItem1.url" type="video/mp4" />
         </video>
     </div>
